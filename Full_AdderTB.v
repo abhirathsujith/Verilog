54370c2fcab7fb88ee5f2ec18c8637a08732120a
Abhirath Sujith <78019581+abhirathsujith@users.noreply.github.com>
2022-08-12 22:03:03 +0530

    Create Full_AdderTB.v

---
+++ Full_AdderTB.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module Full_AdderTB () ;
+reg [3:0]a,b;
+reg c;
+wire [3:0]s;
+wire cout;
+RCA rcl (s, cout, a, b, c) ;
+initial
+begin
+#10 a=4; b=1; c=0;
+#10 a=2; b=4; c=0;
+#50 $stop;
+end
+endmodule
